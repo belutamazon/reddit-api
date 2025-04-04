96b36957a7987b8b4078dfa425557ab855e9057f
Daniel535xxxx16 <danielraharjalim3@gmail.com>
2025-04-04 15:18:18 +0700

    Commit mongoDB, moongoose

--- user.model.js
+++ user.model.js
@@ -1,10 +1,9 @@
-src/api/models/user.model.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-username: { type: String, required: true, unique: true },
-karma: { type: Number, default: 0 },
-created_at: { type: Date, default: Date.now }
+  username: { type: String, required: true, unique: true },
+  karma: { type: Number, default: 0 },
+  created_at: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', UserSchema);
